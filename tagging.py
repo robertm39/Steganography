40fa1e5035c2c366c15acddcb2837ecc99445c31
robertm39 <robertpmorton39@gmail.com>
2021-05-01 18:05:16 -0400

    Now there are 128 check bits

--- tagging.py
+++ tagging.py
@@ -14,10 +14,12 @@ from PIL import Image
 import conversion as conv
 import steganography as stega
 
+#340,282,366,920,938,463,463,374,607,431,768,211,456
+
 # A tag is a sequence of bits at the start of a hidden message
 # that describes the type and layout of the message.
 #
-# The first 64 bits are the check bits.
+# The first 128 bits are the check bits.
 # If these bits have the right pattern,
 # it is almost certain that there is a message.
 #
@@ -100,8 +102,9 @@ def convert_from_image(image):
 TO_BITS_CONVERTER_FROM_TYPE = {TYPE_STRING_TAG: convert_from_string,
                                TYPE_IMAGE_TAG: convert_from_image}
 
-NUM_CHECK_BITS = 64
-CHECK_NUM = 15971532633023303877
+NUM_CHECK_BITS = 128
+# CHECK_NUM = 15971532633023303877
+CHECK_NUM = 301745980665976028475011311407568552610
 CHECK_BITS = tuple(conv.get_bits(CHECK_NUM, width=NUM_CHECK_BITS))
 
 FIELD_FIRST_SEGMENT_LENGTH = 3
@@ -264,105 +267,3 @@ def encode_message(carrier, message, block_size):
     bits = np.array(bits, dtype=np.uint8)
     
     return stega.encode_message(carrier, bits, block_size=block_size)
-
-# def to_bits_with_tag(message):
-#     if isinstance(message, str):
-#         return string_to_bits_with_tag(message)
-#     if isinstance(message, Image.Image):
-#         return image_to_bits_with_tag(message)
-
-# def from_bits_with_tag(bits):
-#     #The first 8 bits tell the type
-#     #The next 24 are the check bits
-#     #The next 32 tell the format
-#     type_bits = bits[:8]
-    
-#     check_bits = bits[8:32]
-#     if check_bits != CHECK_BITS:
-#         raise CheckBitsError()
-    
-#     format_tag = bits[32:64]
-    
-#     # print(type_bits)
-#     # print(format_tag)
-    
-#     rest = bits[64:]
-    
-#     # type_tag = int(''.join(type_bits), 2)
-#     type_tag = conv.bits_to_num(type_bits)
-    
-#     if type_tag == TYPE_STRING_TAG:
-#         length = conv.bits_to_num(format_tag)
-#         num_bits = length*7
-#         # relevant_bits = rest[:num_bits]
-#         relevant_bits = rest
-#         return conv.bits_to_str(relevant_bits, width=7)
-    
-#     if type_tag == TYPE_IMAGE_TAG:
-#         width_bits = format_tag[:14]
-#         height_bits = format_tag[14:28]
-#         depth_bits = format_tag[28:31]
-#         alpha_bit = format_tag[31]
-        
-#         width = conv.bits_to_num(width_bits)
-#         height = conv.bits_to_num(height_bits)
-#         depth = conv.bits_to_num(depth_bits)
-#         add_alpha = bool(alpha_bit)
-        
-#         shape = (width, height, depth)
-#         num_bits = width*height*depth*8
-#         relevant_bits = rest[:num_bits]
-        
-#         return conv.bits_to_image(relevant_bits, shape, add_alpha)
-
-# def string_to_bits_with_tag(string):
-#     #8 bits to tag it as a string
-#     type_bits = conv.get_bits(TYPE_STRING_TAG, width=8)
-#     # print('Type bits:')
-#     # print(type_bits)
-    
-#     #32 bits to tell the length of the string in bits
-#     len_bits = conv.get_bits(len(string), width=32)
-    
-#     bits = conv.str_to_bits(string)
-    
-#     result = type_bits + len_bits + CHECK_BITS + bits
-#     return np.array(result, dtype=np.uint8)
-
-# #May be fragile
-# def get_depth(image):
-#     if image.mode == 'YCbCr':
-#         return 3
-#     if image.mode == 'RGBA': #The alpha channel is removed
-#         return 3
-#     return len(image.mode)
-
-# def image_to_bits_with_tag(image):
-#     #8 bits to tag it as an image
-#     type_bits = conv.get_bits(TYPE_IMAGE_TAG, width=8)
-    
-#     w, h = image.size
-#     d = get_depth(image)
-    
-#     #14 bits to tell the width of the image
-#     width_bits = conv.get_bits(w, width=14)
-    
-#     #14 bits to tell the height of the image
-#     height_bits = conv.get_bits(h, width=14)
-    
-#     #3 bits to tell the depth of the image
-#     depth_bits = conv.get_bits(d, width=3)
-    
-#     #One bit to tell whether an alpha channel needs to be added back
-#     add_alpha = image.mode == 'RGBA'
-#     add_alpha_num = int(add_alpha)
-#     add_alpha_bit = conv.get_bits(add_alpha_num, width=1)
-    
-#     bits = conv.image_to_bits(image, add_alpha)
-    
-#     tag_bits = type_bits + width_bits + height_bits + depth_bits + add_alpha_bit
-#     tag_bits = tag_bits = list(CHECK_BITS)
-#     tag_bits = np.array(tag_bits) 
-    
-#     return np.concatenate([tag_bits, bits], axis=0)
-#     # return tag_bits + bits
